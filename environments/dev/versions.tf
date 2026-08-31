e301e3bd30154eaa0097ed050622f55537d36250
Strickland <jstri01@veritivcorp.com>
2026-08-31 08:14:25 -0400

    Initial Terraform project structure

---
+++ environments/dev/versions.tf
@@ -0,0 +1,33 @@
+#############################################################
+# Terraform Version Requirements
+#
+# This file defines:
+# - Minimum Terraform version
+# - Required providers
+#
+# Think of providers as plugins that allow Terraform
+# to communicate with external systems such as:
+#
+# - VMware vSphere
+# - Kubernetes
+# - AWS
+# - Azure
+#
+#############################################################
+
+terraform {
+
+  required_version = ">= 1.16.0"
+
+  required_providers {
+
+    #########################################################
+    # VMware vSphere Provider
+    #########################################################
+    vsphere = {
+      source  = "hashicorp/vsphere"
+      version = "~> 2.13"
+    }
+
+  }
+}
